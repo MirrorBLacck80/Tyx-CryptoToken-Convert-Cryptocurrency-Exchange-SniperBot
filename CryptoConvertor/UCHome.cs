867b926247e2f101ba0e66d355e9733263d10dfc
sadaxxz <darkarastirma47@outlook.com>
2024-10-14 18:58:55 +0300

    Upload Files

---
+++ CryptoConvertor/UCHome.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CryptoConvertorDemo
+{
+    public partial class UCHome : UserControl
+    {
+        public UCHome()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Converting Started.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show("The hash screen will appear in a few minutes..", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
